24f58cc35d5798622c4c9789b80743e94aaa2c3d
khoi <khoidolce@gmail.com>
2022-03-07 11:05:53 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const person = {
+  name: "Khôi",
+  age: 8,
+  status: false,
+  children: [
+    { id: "1", name: "Hí" },
+    { id: "2", name: "Hú" },
+  ]
+}
+//Sử dụng hàm trong react
+const showAge = (age) => <p>Tủi của bạn nà: {age}</p>
+//Sử dụng component trong react
+const ShowAge = props => <p>Tủi của bạn là: {props.age}</p>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <div>
+    <p>Tên:: {person.name}</p>
+    <p>Tuổi: {person.age}</p>
+    <p>{person.status ? "Đã hứa hôn" : "Chưa kết hôn"}</p>
+    <ul>
+      {person.children.map(child => <li>
+        {child.name}
+      </li>)}
+    </ul>
+    <div>function: {showAge(person.age)}</div>
+    <div>Component: <ShowAge age={person.age} /></div>
+  </div>
+  , document.querySelector("#root"));
