d58efe2fe59ab3430aaf56fac08a24e9827b3e6d
khoi <khoidolce@gmail.com>
2022-03-11 12:03:12 +0700

    buổi 3 - state, props & event

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Box from './components/Box';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("red");
+  const [myStatus, setMyStatus] = useState(false);
+  const [products, setProducts] = useState([{ id: 1, name: "A" }, { id: 2, name: "B" }, { id: 3, name: "C" }])
+  //remove item
+  const removeItem = (id) => {
+    const newProducts = products.filter(item => item.id !== id);
+    setProducts(newProducts);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return <div>
+    <h2>Demo State Basic</h2>
+    Number: {count} <br />
+    String: <Box color={color} /><br />
+    <h2>Demo Event</h2>
+    Boolean: {myStatus ? "Đã nhận được tiền" : "Chưa nhận được tiền"} <br />
+    <hr />
+    Number: {count} <br /> <button onClick={() => setCount(count + 1)}>Click count</button>
+    <button onClick={() => setMyStatus(!myStatus)}>Click Status</button>
+    <br />
+    {myStatus && <div>
+      Arr: {products.map(item => <div>{item.name}
+        <button onClick={() => removeItem(item.id)}>Delete</button>
+      </div>)}
+    </div>}
+    <hr />
+  </div>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,31 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const person = {
-  name: "Khôi",
-  age: 8,
-  status: false,
-  children: [
-    { id: "1", name: "Hí" },
-    { id: "2", name: "Hú" },
-  ]
-}
-//Sử dụng hàm trong react
-const showAge = (age) => <p>Tủi của bạn nà: {age}</p>
-//Sử dụng component trong react
-const ShowAge = props => <p>Tủi của bạn là: {props.age}</p>
-
 ReactDOM.render(
-  <div>
-    <p>Tên:: {person.name}</p>
-    <p>Tuổi: {person.age}</p>
-    <p>{person.status ? "Đã hứa hôn" : "Chưa kết hôn"}</p>
-    <ul>
-      {person.children.map(child => <li>
-        {child.name}
-      </li>)}
-    </ul>
-    <div>function: {showAge(person.age)}</div>
-    <div>Component: <ShowAge age={person.age} /></div>
-  </div>
-  , document.querySelector("#root"));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
