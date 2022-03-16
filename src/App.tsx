c782900fbda1c07a07297c503d820b389843260a
khoi <khoidolce@gmail.com>
2022-03-16 19:57:40 +0700

    buổi 5: feat: create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import ShowInfo from './components/ShowInfo'
+
 import { Products } from './types/products'
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom'
+import { list } from './api/product'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import ProductDetail from './pages/ProductDetail'
+import AdminLayouts from './pages/layouts/Adminlayouts'
+import Dashboard from './pages/Dashboard'
+import WebsiteLayout from './pages/layouts/Websitelayout'
+
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [info, setInfo] = useState<Products>({
-    name: "Khooi",
-    age: 22
-  });
+    const [products, setProducts] = useState<{ _id: number, name: string }[]>([])
 
-  return (
-    <div className="App">
-      {count} <button onClick={() => setCount(count + 1)}>Click</button>
-      <ShowInfo person={info} />
-    </div>
-  )
+    useEffect(() => {
+        const getProducts = async () => {
+            const { data } = await list();
+            setProducts(data);
+        }
+        getProducts();
+    }, [])
+
+    return (
+        <div className="App">
+            <header>
+                <ul>
+                    <li><NavLink to="">Home Page</NavLink></li>
+                    <li><NavLink to="">Product</NavLink></li>
+                    <li><NavLink to="">About</NavLink></li>
+                </ul>
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<WebsiteLayout />}>
+                        <Route index element={<HomePage />} />
+                        <Route path="product" element={<ProductPage />} />
+                        <Route path="/product/:id" element={<ProductDetail />} />
+                    </Route>
+                    <Route path="admin" element={<AdminLayouts />}>
+                        <Route index element={<Navigate to="dashboard" />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                    </Route>
+                </Routes>
+            </main>
+        </div >
+    )
 }
 
 export default App
